fca1bad5972040817538739b3eda822e698ea4f4
Kevin Lamping <klamping@gmail.com>
2019-07-17 00:29:03 -0500

    first commit

---
+++ test/specs/login.js
@@ -0,0 +1,43 @@
+const expect = require('chai').expect;
+
+describe('Login Page', function () {
+    beforeEach(function () {
+        browser.url('./login');
+    });
+
+    it('should let you log in with valid credentials', function () {
+        $('#username').setValue('tomsmith');
+        $('#password').setValue('SuperSecretPassword!');
+        $('button*=Login').click();
+    })
+
+    it.only('should throw error with missing credentials', function () {
+        $('button*=Login').click();
+        browser.debug();
+    })
+
+    it('should throw error with missing password', function () {
+        $('#username').setValue('tomsmith');
+        $('button*=Login').click();
+
+        const errorMessage = $('#flash-messages .error').getText();
+        expect(errorMessage).to.contain('Your password is invalid!')
+    })
+
+    it('should throw error with missing username', function () {
+        $('#password').setValue('SuperSecretPassword!');
+        $('button*=Login').click();
+
+        const errorMessage = $('#flash-messages .error').getText();
+        expect(errorMessage).to.contain('Your username is invalid!')
+    })
+
+    it('should throw error with incorrect password', function () {
+        $('#username').setValue('tomsmith');
+        $('#password').setValue('WrongPassword!');
+        $('button*=Login').click();
+
+        const errorMessage = $('#flash-messages .error').getText();
+        expect(errorMessage).to.contain('Your password is invalid!')
+    })
+})
